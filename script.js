b45b6278355cda23d3c21f2d355b272ca9dcab49
gkourako <georgekourakos4@gmail.com>
2020-08-04 22:49:42 -0400

    Added variables to script.js

--- script.js
+++ script.js
@@ -1,65 +1,49 @@
+
 // Variables
+var answer1 = document.getElementById(answer1)
+var answer2 = document.getElementById(answer2)
+var answer3 = document.getElementById(answer3)
+var answer4 = document.getElementById(answer4)
+var question = []
 
-var quizContainer = document.getElementById('quiz')
-var resultsContainer = document.getElementById('results')
-var 
-var timeEl = document.getElementById("time");
-var question = [
+// Question Variable
+var questionArr = [
 {
-  question: "",
-  answer:"2020",
-  answerlist: [1920, 1969, 2020,],
-  correctAnswer: 3
-  }
-]
+  question: "What car did Brian drive in Fast and Furious",
+  answer:"Blue R34",
+  answerlist: ["Blue R34", "Yellow Supra", "Black Duragno","Silver R34"],
+  correctAnswer: 0
+  },
 
-var question = [
   {
-    question: "",
-    answer:"",
-    answerlist: ["yes", "no", "I should","asdf"],
+    question: "What is Dom and Letty's famous saying ",
+    answer:"Ride or Die",
+    answerlist: ["I live life a quarter mile at a time", 
+    "Ride or Die", 
+    "It doesn't matter whether you win by an inch or a mile, winning is winning",
+    "You know this ain't no 10-second race.” “I've got nothing but time."],
+    correctAnswer: 2
+  },
+  {
+    question: "In Toyko Drift who says, Life's simple: You make choices and you don't look back.",
+    answer:"Han",
+    answerlist: ["Drift King", "Sean", "D.K.", "Han"],
+    correctAnswer: 3
+  },
+  {
+    question: "Which movie does Dom look at Letty and say, Ride or die, Remember?",
+    answer:"Fast 6",
+    answerlist: ["2 Fast 2 Furious", "Fast 6", "Fate of the Furious", " The Fast and Furious"],
+    correctAnswer: 1
+  },
+  {
+    question: "What is Paul Walkers most famous quote?",
+    answer:"If one day the speed kills me don’t be sad because i will have died smiling.",
+    answerlist: ["If one day the speed kills me don’t be sad because i will have died smiling.",
+    "I don’t care what it is in life: listen to your heart. If you do, no matter what, you win.",
+    "I’m not the kind of guy who’s taking advantage of my position.",
+    "You know, all that really matters is that the people you love are happy and healthy. Everything else is just sprinkles on the sundae."],
     correctAnswer: 3
   }
 ]
-
-var question = [
-  {
-    question: "What's your favorite food",
-    answer:"",
-    answerlist: [,,,],
-    correctAnswer: 3
-  }
-]
-  
-var question = [
-  {
-    question: "",
-    answer:"",
-    answerlist: [,,,],
-    correctAnswer: 3
-  }
-]
-  
-var question = [
-  {
-    question: "",
-    answer:"",
-    answerlist: [,,,],
-    correctAnswer: 3
-  }
-]
-  
-var secondsLeft = 10;
-
-function setTime() {
-  var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timeEl.textContent = secondsLeft + " seconds left till reading starts";
-
-    if(secondsLeft === 0) {
-      clearInterval(timerInterval);
-      sendMessage();
-    }
-
-  }, 1000);
-}
+function workplz 
